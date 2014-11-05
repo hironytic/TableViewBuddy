ac79dafdee071c8dcc063352af8a594cef6f2a46
Hironori Ichimiya <hiron@hironytic.com>
2014-11-06 08:45:48 +0900

    wrote comments for public properties and methods in TBTableData, TBTableDataSection and TBTableDataRow. (refs #3)

--- TableViewBuddy/TBTableDataRow.h
+++ TableViewBuddy/TBTableDataRow.h
@@ -29,23 +29,93 @@
 @class TBTableDataInitializationContext;
 @class TBTableDataSection;
 
+/**
+ * `TBTableDataRow` represents a data model of the row as a part of the table data.
+ */
 @interface TBTableDataRow : NSObject
 
+/**
+ * A `TBTableDataSection` object to which this object belongs.
+ */
 @property(nonatomic, weak, readonly) TBTableDataSection *section;
+
+/**
+ * A reuse identifier of the corresponding table cell.
+ */
 @property(nonatomic, copy, readonly) NSString *reuseIdentifier;
+
+/**
+ * A boolean value that determines whether the row is hidden.
+ */
 @property(nonatomic, assign, readonly) BOOL hidden;
+
+/**
+ * A boolean value that determines whether the row is enabled.
+ */
 @property(nonatomic, assign, readonly) BOOL enabled;
 
+/**
+ * Returns a generator block object, which can be used as a last parameter in `[TBTableDataSection insertRowAfter:withContext:generator:]`.
+ *
+ * @param configurator A block object to configure the row.
+ * @return The generator block object.
+ */
 + (TBTableDataRow *(^)(TBTableDataInitializationContext *context))rowGeneratorWithConfigurator:(void (^)(TBTableDataInitializationContext *context))configurator;
 
+/**
+ * Returns a `TBTableDataRow` object initialized with specified context.
+ *
+ * This is the designated initializer method.
+ *
+ * @param context A context object to initialize.
+ * @return The row object.
+ */
 - (instancetype)initWithContext:(TBTableDataInitializationContext *)context;
 
+/**
+ * Returns an index path of the row in the table view.
+ *
+ * Indices in the returned path are indices in the visible sections/rows. Hidden sections/rows are not counted.
+ *
+ * @return The index path, or nil when this row is not visible.
+ */
 - (NSIndexPath *)rowIndexPath;
+
+/**
+ * Finds a corresponding table cell and returns it.
+ *
+ * @return The table cell object, or nil when it is not found.
+ */
 - (UITableViewCell *)findVisibleCell;
 
+/**
+ * Changes the visibility of the row.
+ *
+ * @param hidden `YES` to hide the row, or `NO` to show.
+ * @param context A context object. `TBTableDataInitializationContext` or `TBTableDataUpdateContext` object is required.
+ */
 - (void)setHidden:(BOOL)hidden withContext:(TBTableDataContext *)context;
+
+/**
+ * Enables or disables the row.
+ *
+ * @param enabled `YES` to enable the row, or `NO` to disable.
+ * @param context A context object.
+ */
 - (void)setEnabled:(BOOL)enabled withContext:(TBTableDataContext *)context;
+
+/**
+ * Reloads the row in table view.
+ *
+ * @param context A context object. `TBTableDataUpdateContext` object is required.
+ */
 - (void)reloadWithContext:(TBTableDataContext *)context;
+
+/**
+ * Deletes the row.
+ *
+ * @param context A context object. `TBTableDataInitializationContext` or `TBTableDataUpdateContext` object is required.
+ */
 - (void)deleteWithContext:(TBTableDataContext *)context;
 
 // for subclass overriding
