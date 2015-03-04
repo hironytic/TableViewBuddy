fe9ecbeda73618677ec8e443471730128586c2ec
Hironori Ichimiya <hiron@hironytic.com>
2015-03-05 08:25:44 +0900

    Update copyright year

--- TableViewBuddy/internal/TBTableData_Internal.h
+++ TableViewBuddy/internal/TBTableData_Internal.h
@@ -2,7 +2,7 @@
 // TBTableData_Internal.h
 // TableViewBuddy
 //
-// Copyright (c) 2014 Hironori Ichimiya <hiron@hironytic.com>
+// Copyright (c) 2014,2015 Hironori Ichimiya <hiron@hironytic.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
